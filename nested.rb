8503d1c82e42447eb62df9430b1ea79bfa96c85b
Yani Asari <yani.iman1@gmail.com>
2020-08-09 03:41:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
         }
      }
 programmer_hash.merge(:yukihiro_matsumoto => {
- 		      :known_for => "Ruby", 
- 		      :languages => ["LISP, "C"]
- 		      })
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ })
     
 end
 
